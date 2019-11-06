3a56acb7a9500f1a720d063b65d1a9317027066f
Oleg <oleg.voskoboev@gmail.com>
2019-11-06 21:05:43 +0200

    Initial commit

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    private int[] _slices = new int[6];
+
+    private void Awake()
+    {
+        FindObjectOfType<GameManager>().AddCircle(this);
+    }
+    public bool IsSliceFitting(List<Slice> bigSlice)
+    {
+        foreach (Slice slice in bigSlice)
+        {
+            if (slice.Position >= 0 && (int)slice.Position < _slices.Length)
+            {
+                if (_slices[(int)slice.Position] != 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                Debug.Log("Wrong position value");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void PutSliceInCircle()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Slice.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Slice : MonoBehaviour
+{
+    public Position Position { get; private set; } = Position.First;
+
+    public void Init(Position position)
+    {
+        Position = position;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 30 + 60 * (int)Position) * -1);
+    }
+}
+
+public enum Position
+{
+    First, Second, Third, Fourth, Fifth, Sixth
+}
